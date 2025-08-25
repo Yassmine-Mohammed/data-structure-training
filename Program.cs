943cc0f6a92934e63c08977d705793d02713407b
Yassmine Mohammed <yasmyn.ftha00671@compit.aun.edu.eg>
2025-08-25 15:24:38 +0300

    Implemented stack using array in C#

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace datastructurebasics
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            /* 
+            Stack : 1) Array based implementation of the ADT stack ------------------------------------------------
+
+            Stack stack = new Stack();
+            stack.Push(1);          // 1
+            stack.Push(2);          // 2
+            stack.Push(3);          // 3
+            stack.Print();
+            stack.GetTheTop();
+
+            int theDeletedElement = 0;
+            stack.Pop(ref theDeletedElement); // remove 3
+            Console.WriteLine("the top has been deleted sucessfully");
+            stack.Print();
+            stack.GetTheTop();
+            */
+
+        }
+    }
+}
---
+++ Stack.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace datastructurebasics
+{
+   /* Stack : 1) Array based implementation of the ADT stack
+   
+    public class Stack
+    {
+        int top;
+        const int maxSize = 100;
+        int[] stackArray = new int[maxSize];
+        public Stack()
+        {
+            top = -1;
+        }
+        public void Push(int Elemnt)
+        {
+            if (top >= maxSize - 1)
+            {
+                Console.WriteLine("Stack Overflow");
+            }
+            else
+            {
+                stackArray[++top] = Elemnt;
+            }
+        }
+        // remove the top
+        public int Pop()
+        {
+            if (top < 0)
+            {
+                Console.WriteLine("Stack Underflow");
+                return -1; // or throw an exception
+            }
+            else
+            {
+                return stackArray[top--];
+            }
+        }
+        // save a copy & remove the top 
+        public void Pop(ref int Element)
+        {
+            if (top < 0)
+            {
+                Console.WriteLine("Stack Underflow");
+            }
+            else
+            {
+                stackArray[top--] = Element;
+            }
+        }
+        public void GetTheTop()
+        {
+            Console.WriteLine("The top of the stack is:"+stackArray[top]);
+        }
+        public void Print ()
+        {
+            for (int i = 0; i <top +1; i++)
+            { 
+                Console.WriteLine(i + ": " + stackArray[i]);            
+            }
+        }
+    }
+   */
+            
+}
