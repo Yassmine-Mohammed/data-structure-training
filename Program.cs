3014494daab81b28ee6b3d1a74c5402a3895fd49
Yassmine Mohammed <yasmyn.ftha00671@compit.aun.edu.eg>
2025-08-26 17:46:02 +0300

    Update Program.cs
     Stack's uses # Balanced Parentheses Using Stack {([ ])}

--- Program.cs
+++ Program.cs
@@ -1,28 +1,57 @@
-﻿using System;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Reflection.Metadata.Ecma335;
 
 namespace datastructurebasics
 {
     public class Program
     {
+        static bool arepair(char open, char close)
+        {
+            if (open == '(' && close == ')') { return true; }
+            else if (open == '{' && close == '}') { return true; }
+            else if (open == '[' && close == ']') { return true; }
+            return false;
+        }
+        static bool arebalanced(string exp)
+        {
+            Stack<char> s = new Stack<char>();
+            for (int i = 0; i < exp.Length; i++)
+            {
+                if (exp[i] == '(' || exp[i] == '{' || exp[i] == '[')
+                {
+                    s.Push(exp[i]);
+                }
+                else if (exp[i] == ')' || exp[i] == '}' || exp[i] == ']')
+                {
+                    if (s.Count == 0||!arepair(s.Peek(), exp[i]))
+                    {
+                        return false;
+                    }
+                    else
+                    {
+                        s.Pop();
+                    }
+                }
+            }
+        
+    
+            return (s.Count == 0 );
+        }
         static void Main(string[] args)
         {
-            /* 
-            Stack : 1) Array based implementation of the ADT stack ------------------------------------------------
-
-            Stack stack = new Stack();
-            stack.Push(1);          // 1
-            stack.Push(2);          // 2
-            stack.Push(3);          // 3
-            stack.Print();
-            stack.GetTheTop();
-
-            int theDeletedElement = 0;
-            stack.Pop(ref theDeletedElement); // remove 3
-            Console.WriteLine("the top has been deleted sucessfully");
-            stack.Print();
-            stack.GetTheTop();
-            */
-
+            Console.Write("enter you experation: ");
+            string exp = Console.ReadLine();
+            if (arebalanced(exp))
+            {
+                Console.WriteLine("The expression is balanced");
+            }
+            else
+            {
+                Console.WriteLine("The expression is not balanced");
+            }
         }
     }
 }
