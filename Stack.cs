0e69e1f613a27cadd8e6dc424e04cc7c9d11aa53
Yassmine Mohammed <yasmyn.ftha00671@compit.aun.edu.eg>
2025-08-26 17:44:44 +0300

    Update Stack.cs
    Stack implement using Linked List

--- Stack.cs
+++ Stack.cs
@@ -1,13 +1,15 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace datastructurebasics
 {
-   /* Stack : 1) Array based implementation of the ADT stack
-   
+    //Stack : 1) Array based implementation of the ADT stack
+
+    /*
     public class Stack
     {
         int top;
@@ -57,14 +59,103 @@ namespace datastructurebasics
         {
             Console.WriteLine("The top of the stack is:"+stackArray[top]);
         }
+        // Print stack items
         public void Print ()
         {
-            for (int i = 0; i <top +1; i++)
+            Console.Write("[ ");
+            for (int i = top; i >= 0; i--)
             { 
-                Console.WriteLine(i + ": " + stackArray[i]);            
+                Console.Write(stackArray[i]+" ");            
             }
+            Console.Write("]");
+
+        }
+
+    }
+    */
+   
+    //========================================================================================================================
+
+    // stack 2) Linked list impelemnt 
+    /*
+    class Node <T>
+    {
+        public T Data;
+        public Node<T> Next;
+
+        public Node(T data)
+        {
+            Data = data;
+            Next = null;
         }
     }
-   */
-            
+
+    class StackLinkedList<T>
+    {
+        private Node<T> top;
+
+        public StackLinkedList()
+        {
+            top = null;
+        }
+
+        // Push: إضافة عنصر فوق
+        public void Push(T data)
+        {
+            Node<T> newNode = new Node<T>(data);    // إنشاء عقدة جديدة
+            if(newNode == null)
+            {
+                Console.Write("Stack push cannot allocate memory."); 
+            }
+            else 
+            { 
+            newNode.Next = top;                     // ربط العقدة الجديدة بالعقدة العلوية الحالية
+            top = newNode;                          // تحديث العقدة العلوية لتكون العقدة الجديدة
+            }
+        }
+
+        // Check if empty
+        public bool IsEmpty()
+        {
+            return top == null;
+        }
+
+        // Pop: إزالة العنصر العلوي
+        public T Pop()
+        {
+            if (IsEmpty())
+                throw new InvalidOperationException("Stack is empty!");
+
+            Node<T> temp = top; // حفظ العقدة العلوية مؤقتًا
+            top = top.Next;     // تحديث العقدة العلوية لتكون العقدة التالية
+            return temp.Data;   // إرجاع بيانات العقدة التي تمت إزالته
+        }
+       
+        // Peek: إرجاع العنصر العلوي بدون إزالته
+        public T Peek()
+        {
+            if (IsEmpty())
+                throw new InvalidOperationException("Stack is empty!");
+
+            return top.Data;
+        }
+
+        public void display()
+        {
+            Node<T> current = top;
+            Console.WriteLine("Stack elements:");
+            Console.Write(" [");
+            while (current != null)
+            {
+                Console.Write(current.Data +" ");
+                current = current.Next;
+            }
+            Console.WriteLine("].");
+
+        }
+
+    }
+    */
+
+
 }
